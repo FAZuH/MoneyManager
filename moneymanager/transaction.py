1ec558fc700ce6c091bba7e9acd1c886e4ded226
FAZuH <fazuhhh@gmail.com>
2024-06-15 16:56:50 +0700

    Add Transaction class

---
+++ moneymanager/transaction.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from typing import Literal
+
+
+class Transaction:
+# no;date;account;amount;type;category;comment
+    def __init__(
+        self,
+        no: int,
+        date: datetime,
+        account: str,
+        amount: float,
+        type_: Literal["expense", "income"],
+        category: str,
+        comment: str
+    ) -> None:
+        self.no = no
+        self.date = date
+        self.account = account
+        self.amount = amount
+        self.type_ = type_
+        self.category = category
+        self.comment = comment
+
