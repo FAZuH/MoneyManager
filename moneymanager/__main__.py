36a11ba10704a96cfc73882e3be3b8e5defa9006
FAZuH <fazuhhh@gmail.com>
2024-06-15 16:14:27 +0700

    Add base main option flow

--- moneymanager/__main__.py
+++ moneymanager/__main__.py
@@ -1,5 +1,24 @@
+import display
+
+
 def main() -> None:
-    pass
+    display.display_welcome()
+
+    while True:
+        option = display.prompt_main()
+        match option:
+            case 1:  # input expense
+                display.prompt_input_expense()
+            case 2:  # input income
+                display.prompt_input_income()
+            case 3:  # view balance
+                display.prompt_view_balance()
+            case 4:  # expense statistics
+                display.prompt_expense_statistics()
+            case 5:  # income statistics
+                display.prompt_income_statistics()
+            case _:
+                print("Invalid input.")
 
 
 if __name__ == "__main__":
