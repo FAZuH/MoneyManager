d3f22c14639d5321ee49c58fd584510a30661872
AdrianSPratama <16523037@std.stei.itb.ac.id>
2024-06-17 11:55:19 +0700

    feat: add nested add transaction

--- moneymanager/__main__.py
+++ moneymanager/__main__.py
@@ -2,13 +2,13 @@ import display
 import csv_manager
 
 
-def _do_input_expense():
-    expense = display.input_transaction("expense")
-    csv_manager.insert_transaction(expense)
+# def _do_input_expense():
+#     expense = display.input_transaction("expense")
+#     csv_manager.insert_transaction(expense)
 
-def _do_input_income():
-    income = display.input_transaction("income")
-    csv_manager.insert_transaction(income)
+# def _do_input_income():
+#     income = display.input_transaction("income")
+#     csv_manager.insert_transaction(income)
 
 def _do_view_balance():
     return
@@ -26,16 +26,26 @@ def main() -> None:
     while True:
         option = display.input_main()
         match option:
-            case 1:  # input expense
-                _do_input_income()
-            case 2:  # input income
-                _do_input_expense()
-            case 3:  # view balance
+            case 1:  # Add transaction
+                option = display.must_get_input(
+                    "Input expense or income?"
+                    "1. Expense"
+                    "2. Income", int
+                    )
+                match option:
+                    case 1: # Input expense
+                        new_transaction = display.input_transaction("expense")
+                        csv_manager.insert_transaction(new_transaction)
+                    case 2: # Input income
+                        new_transaction = display.input_transaction("income")
+                        csv_manager.insert_transaction(new_transaction)
+
+            case 2:  # Transaction history
+                display.show_transaction_history()
+            case 3:  # Account
                 _do_view_balance()
-            case 4:  # expense statistics
+            case 4:  # Statistics
                 _do_expense_statistics()
-            case 5:  # income statistics
-                _do_income_statistics()
             case _:
                 print("Invalid input.")
 
