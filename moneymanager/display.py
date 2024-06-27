f18bcb18a23bf51bb6bdefc9a161349a539f9bde
FAZuH <fazuhhh@gmail.com>
2024-06-28 01:22:19 +0700

    feat: add & fix functions on display.py

--- moneymanager/display.py
+++ moneymanager/display.py
@@ -1,25 +1,12 @@
 from datetime import datetime
-from typing import Callable
-from typing import Literal
+import os
+import platform
+from typing import Callable, Literal
 
 from moneymanager.transaction import Transaction
 
 
-def exittable_input(prompt: str) -> str:
-    """Asks the user for an input. Exits the program if the input is 'exit'
-
-    Args:
-        prompt (str): The message to show to the user while asking for an input
-
-    Returns:
-        str: The user's input
-    """
-    inpt = input(prompt)
-    if inpt == "exit":
-        exit()
-    return inpt
-
-def must_get_input[T](prompt: str, type_strategy: Callable[[str], T]) -> T:
+def __must_get_input[T](prompt: str, type_strategy: Callable[[str], T]) -> T:
     """Prompts the user for an input. This function makes sure the user passes a valid 
     input parseable by type_strategy, then returning the parsed value.
 
@@ -31,18 +18,40 @@ def must_get_input[T](prompt: str, type_strategy: Callable[[str], T]) -> T:
         T: The user input parsed into type 'T' by type_strategy
     """
     while True:
-        inpt = exittable_input(prompt)
+        inpt = input(prompt)
+
+        if inpt == "exit":
+            exit()
+
         try:
             return type_strategy(inpt)
         except ValueError:
             print("Please enter a valid input.")
 
-def display_welcome() -> None:
+
+def clear_view() -> None:
+    current_os = platform.system()
+    if current_os == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+#####
+# SHOW: Just prints something to the terminal
+#####
+
+def show_welcome() -> None:
     """Displays a welcome message to the user."""
     # TODO: Display ASCII art of 'MoneyManager'
     print("Welcome to MoneyManager")
 
-def input_main() -> int:
+
+#####
+# CHOOSE: Prompts the user to choose an option
+#####
+
+def choose_main() -> int:
     """Prompts the user for main option.
 
     Returns:
@@ -55,29 +64,42 @@ def input_main() -> int:
         "2. Transaction History"
         "3. Account"
         "4. Statistics"
+        "5. Exit"
     )
-    inpt = exittable_input('> ')
-    parsed_input = must_get_input(inpt, int)
+    inpt = input('> ')
+    parsed_input = __must_get_input(inpt, int)
     return parsed_input
 
-def input_transaction(type_: Literal['expense'] | Literal['income']) -> Transaction:
+def choose_transaction_type() -> int:
+    print(
+        "Choose transaction type:"
+        "------------------------"
+        "1. Expense"
+        "2. Income"
+        "3. Back"
+    )
+    inpt = input('> ')
+    parsed_input = __must_get_input(inpt, int)
+    return parsed_input  # type: ignore
+
+
+#####
+# INPUT: Prompts the user for a more complete input
+#####
+
+def input_transaction(type_: Literal["expense", "income"]) -> Transaction:
     """Asks user input for adding transaction
     
     Args:
         type_ (str): Transaction type 
     """
-    account = must_get_input("Input account: ", str)
-    category = must_get_input("Input category: ", str)
-    inpt = must_get_input(f"Input {type_}: ", float)
-    comment = must_get_input("Input comment: ", str)
-    transaction = Transaction(datetime.now(), account, inpt, type_, category, comment)
+    account = __must_get_input("Input account: ", str)
+    category = __must_get_input("Input category: ", str)
+    inpt = __must_get_input(f"Input {type_}: ", float)
+    comment = __must_get_input("Input comment: ", str)
+
+    # TODO: how should i do this
+    # option 1: return list of answer. create Transaction object on controller (__main__)
+    # option 2: create transaction object here, along with the id (increases code coupling)
+    transaction = Transaction(1, datetime.now(), account, inpt, type_, category, comment)
     return transaction
-
-def view_account_balance(account: str):
-    ...
-
-def expense_statistics():
-    ...
-
-def income_statistics():
-    ...
