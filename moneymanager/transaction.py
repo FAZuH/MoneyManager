7803e39bbe7e563dab9ccc24bcad8a9ac28d3a1b
FAZuH <fazuhhh@gmail.com>
2024-06-28 01:20:27 +0700

    feat: add from_list and to_list method to Transaction

--- moneymanager/transaction.py
+++ moneymanager/transaction.py
@@ -1,11 +1,13 @@
+from __future__ import annotations
 from datetime import datetime
 from typing import Literal
 
 
 class Transaction:
-# no;date;account;amount;type;category;comment
+
     def __init__(
         self,
+        id_: int,
         date: datetime,
         account: str,
         amount: float,
@@ -13,10 +15,36 @@ class Transaction:
         category: str,
         comment: str
     ) -> None:
+        self.id = id_
         self.date = date
         self.account = account
         self.amount = amount
         self.type_ = type_
         self.category = category
         self.comment = comment
+
+    def to_list(self) -> list[str]:
+        ret = [
+            str(self.id),
+            self.date.isoformat(),
+            self.account,
+            str(self.amount),
+            self.type_,
+            self.category,
+            self.comment
+        ]
+        return ret
+
+    @classmethod
+    def from_list(cls, transaction_list: list[str]) -> Transaction:
+        ret = cls(
+            int(transaction_list[0]),
+            datetime.fromisoformat(transaction_list[1]),
+            transaction_list[2],
+            float(transaction_list[3]),
+            transaction_list[4],  # type: ignore
+            transaction_list[5],
+            transaction_list[6]
+        )
+        return ret
 
