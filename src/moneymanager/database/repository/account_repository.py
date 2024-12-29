4690c893917403efcaf8d4bea225847e9c708f31
FAZuH <fazuhhh@proton.me>
2024-12-29 17:51:12 +0700

    fix: fix ImportError on AccountRepository

--- src/moneymanager/database/repository/account_repository.py
+++ src/moneymanager/database/repository/account_repository.py
@@ -1,19 +1,12 @@
-from typing import TYPE_CHECKING, Any
-
 from moneymanager.database.repository.repository import Repository
-
-if TYPE_CHECKING:
-    from moneymanager.database.entity.account import Account
+from moneymanager.database.entity.account import Account
 
 
 class AccountRepository(Repository[Account, str]):
     def select(self, identifier: str) -> Account: ...
 
-    def insert(self, entity: Account) -> None:
-        return
+    def insert(self, entity: Account) -> None: ...
 
-    def update(self, entity: Account) -> None:
-        return
+    def update(self, entity: Account) -> None: ...
 
-    def delete(self, identifier: str) -> None:
-        return
+    def delete(self, identifier: str) -> None: ...
