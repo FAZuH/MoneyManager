e607c9009adeeb0b47964a15439421f1e9c686ef
FAZuH <fazuhhh@proton.me>
2024-12-29 16:48:43 +0700

    refactor: organize TransactionRepository method order

--- src/moneymanager/database/repository/transaction_repository.py
+++ src/moneymanager/database/repository/transaction_repository.py
@@ -7,11 +7,14 @@ from moneymanager.database.repository.repository import Repository
 class TransactionRepository(Repository[Transaction, str]):
     TRANSACTION_HISTORY_PATH: str = "userdata/transaction_history.csv"
 
-    def select_all(self) -> list[Transaction]:
+    def select(self, identifier: str) -> Transaction:
         with open(self.TRANSACTION_HISTORY_PATH) as stream:
             transactions_data = csv.reader(stream)
-            transactions_list = list(transactions_data)
-            return [Transaction.from_list(transaction) for transaction in transactions_list]
+
+            for transaction in transactions_data:
+                if transaction[0] == identifier:
+                    return Transaction.from_list(transaction)
+        raise ValueError(f"Transaction with id {identifier} not found")
 
     def insert(self, entity: Transaction) -> None:
         row = entity.to_list()
@@ -25,14 +28,11 @@ class TransactionRepository(Repository[Transaction, str]):
     def delete(self, identifier: str) -> None:
         pass
 
-    def select(self, identifier: str) -> Transaction:
+    def select_all(self) -> list[Transaction]:
         with open(self.TRANSACTION_HISTORY_PATH) as stream:
             transactions_data = csv.reader(stream)
-
-            for transaction in transactions_data:
-                if transaction[0] == identifier:
-                    return Transaction.from_list(transaction)
-        raise ValueError(f"Transaction with id {identifier} not found")
+            transactions_list = list(transactions_data)
+            return [Transaction.from_list(transaction) for transaction in transactions_list]
 
     def _read_transaction_history(self) -> list[list[str]]:
         with open(self.TRANSACTION_HISTORY_PATH) as stream:
