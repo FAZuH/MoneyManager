06e6590cabfd90cd1e06c5e9b439c29be465c7b7
AdrianSPratama <16523037@std.stei.itb.ac.id>
2025-01-06 11:31:11 +0700

    feat method requested for this branch

--- src/moneymanager/database/repository/account_repository.py
+++ src/moneymanager/database/repository/account_repository.py
@@ -4,13 +4,46 @@ from moneymanager.database.repository.repository import Repository
 
 
 class AccountRepository(BaseCsvRepository, Repository[Account, str]):
-    def select(self, identifier: str) -> Account: ...
+    def select(self, identifier: str) -> Account:
+        # try:
+        with self.enter_reader() as reader:
+            rows = list(reader)
+            for i in range(len(rows)):
+                if rows[i]["name"] == identifier:
+                    return self.model(rows[i]["name"], float(rows[i]["balance"]))
+        # If account with name identifier not found
+        raise ValueError(f"Account with name {identifier} not found") 
 
-    def insert(self, entity: Account) -> None: ...
+    def insert(self, entity: Account) -> None: # Method for inserting a new account
+        with self.enter_reader() as reader:
+            rows = list(reader) # Turns into a list
+            for i in range(len(rows)):
+                if rows[i]["name"] == entity.name: # If account name is already exist
+                    raise ValueError(f"Account with name {entity.name} is already exist!")
+        # If the account hasn't been created        
+        with self.enter_writer() as writer:
+            writer.writerow({"name": entity.name, "balance": entity.balance})
 
-    def update(self, identifier: str, entity: Account) -> None: ...
+    def update(self, identifier: str, entity: Account) -> None: 
+        with self.enter_reader() as reader:
+            rows = list(reader)
+            for i in range(len(rows)):
+                if rows[i]["name"] == identifier: # If found
+                    with self.enter_writer() as writer:
+                        writer.writerow({"name": entity.name, "balance": entity.balance})
+        # If the account doesn't exist
+        raise ValueError(f"Account with name {identifier} is not exist!")
 
-    def delete(self, identifier: str) -> None: ...
+    def delete(self, identifier: str) -> None:
+        with self.enter_reader() as reader:
+            rows = list(reader)
+            for i in range(len(rows)):
+                if rows[i]["name"] == identifier: # If found
+                    with self.enter_writer() as writer:
+                        writer.writerow({"name": None, "balance": None})
+        # If the account doesn't exist
+        raise ValueError(f"Account with name {identifier} is not exist!")
+
     def select_all(self) -> list[Account]:
         with self.enter_reader() as reader:
             return [self.model(**row) for row in reader]  # type: ignore
