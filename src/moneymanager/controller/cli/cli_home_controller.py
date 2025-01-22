740dc5d3a064755df899dabda8dc7cb4fffacb2c
AdrianSPratama <16523037@std.stei.itb.ac.id>
2025-01-22 21:50:01 +0700

    feat: UserExit and UserCancel handling in home
    UserExit and UserCancel handling in home controller. If exit then closes the program, if cancel then invalid input because it is in home.

--- src/moneymanager/controller/cli/cli_home_controller.py
+++ src/moneymanager/controller/cli/cli_home_controller.py
@@ -4,6 +4,8 @@ from typing import override, TYPE_CHECKING
 
 from moneymanager.controller.base_controller import BaseController
 from moneymanager.view.cli.cli_home_view import CliHomeView
+from moneymanager.exception import UserExit
+from moneymanager.exception import UserCancel
 
 if TYPE_CHECKING:
     from moneymanager.app.app import App
@@ -19,23 +21,29 @@ class CliHomeController(BaseController):
         self._view.display_welcome()
 
         while True:
-            option = self._view.prompt_menu()
-            match option:
-                case 1:  # Add expense
-                    self.app.main_controller.run_add_expense_controller()
-                case 2:  # Add income
-                    self.app.main_controller.run_add_income_controller()
-                    pass
-                case 3:  # Transaction history
-                    # self.app.main_controller.run_()
-                    pass
-                case 4:  # Manage account
-                    self.app.main_controller.run_manage_account_controller()
-                case 5:  # Statistics
-                    # self.app.main_controller.run_()
-                    pass
-                case 6:  # Exit
-                    self._view.display_exit_message()
-                    break
-                case _:
-                    self._view.display_error("Invalid option")
+            try: # Try
+                option = self._view.prompt_menu()
+                match option:
+                    case 1:  # Add expense
+                        self.app.main_controller.run_add_expense_controller()
+                    case 2:  # Add income
+                        self.app.main_controller.run_add_income_controller()
+                        pass
+                    case 3:  # Transaction history
+                        # self.app.main_controller.run_()
+                        pass
+                    case 4:  # Manage account
+                        self.app.main_controller.run_manage_account_controller()
+                    case 5:  # Statistics
+                        # self.app.main_controller.run_()
+                        pass
+                    case 6:  # Exit
+                        self._view.display_exit_message()
+                        break
+                    case _:
+                        self._view.display_error("Invalid option")
+            except UserExit: # If user enter "exit", it will exit
+                self._view.display_exit_message()
+                break
+            except UserCancel: # If user enter "cancel", it's invalid
+                self._view.display_error("Invalid option")
